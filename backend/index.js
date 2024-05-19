0e2efc643c01855fb0df597ad7d525c3e3fd43f0
Robert Navarro <robertlorenzonavarrovalle2@gmail.com>
2024-05-18 23:22:07 -0400

    eliminando la carpeta backend

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const http= require('http')
-
- http.createServer (function (request, response)  {
-    response.write('hola mundo')
-    response.end()
-}).listen(3000)
-
-
-console.log('corriendo en puerto 3000')
